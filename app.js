4807b6b3dc101f39c77cee0bb915eb2cb79a87fa
taoxiaofeng55 <taoxiaofeng55@qq.com>
2018-07-23 20:56:33 +0800

    事件

--- app.js
+++ app.js
@@ -47,13 +47,58 @@
  * require 引用这个模块
  */
 
- 
+
 // var counter = require('./counter');
-var stuff = require('./counter');
+// var stuff = require('./counter');
 
-// var pi = require('./counter').pi;
+// // var pi = require('./counter').pi;
 
 
-console.log(stuff.counter(['ruby', 'nodejs', 'react']));
-console.log(stuff.adder(3));
+// console.log(stuff.counter(['ruby', 'nodejs', 'react']));
+// console.log(stuff.adder(3));
 // console.log(pi);
+
+/**
+ * events 事件
+ */
+
+var events = require('events');
+var util = require('util');
+
+var Person = function (name) {
+    this.name = name;
+}
+
+//Person 继承 events.EventEmitter
+util.inherits(Person, events.EventEmitter);
+
+
+//新建三个对象
+var xiaoming = new Person('xiaoming');
+var lili = new Person('lili');
+var lucy = new Person('lucy');
+
+//把三个对象封装到数组中
+var person = [xiaoming, lili, lucy];
+
+//循环给三个对象绑定事件
+person.forEach(function (person) {
+    person.on('speak', function (message) {
+        console.log(person.name + ' said: ' + message);
+    })
+})
+
+//触发事件
+xiaoming.emit('speak', 'hi');
+lucy.emit('speak', 'I want a curry');
+
+// //新建事件对象
+// var myEmitter = new events.EventEmitter();
+
+// //绑定事件
+// myEmitter.on('someEvent', function (message) {
+//     console.log(message);
+// });
+
+// //触发事件
+// myEmitter.emit('someEvent', 'the event was emitted');
