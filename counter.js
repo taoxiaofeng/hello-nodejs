d850b6d08a4229afaa161648a708f8165a5699ba
taoxf <taoxf@ipharmacare.net>
2019-02-17 14:42:10 +0800

    读写文件（同步，异步）

--- counter.js
+++ counter.js
@@ -2,21 +2,47 @@
 //     return 'three are' + arr.length + 'elements in  the array';
 // }
 
-var adder = function (a) {
-    return `the sum of the 2 number is ${a+pi}`;
-}
+// var adder = function (a) {
+//     return `the sum of the 2 number is ${a+pi}`;
+// }
 
-var pi = 3.14;
+// var pi = 3.14;
 
 // module.exports.counter = counter;
 // module.exports.adder = adder;
 // module.exports.pi = pi;
 
 //改造
+// module.exports = {
+//     counter: function (arr) {
+//         return 'three are' + arr.length + 'elements in  the array';
+//     },
+//     adder: adder,
+//     pi: pi
+// }
+
+// var counter = function(arr) {
+//     return "There are " + arr.length + " element in the array"
+// }
+
+// var adder = function(a, b) {
+//     return `the sum of 2 number is ${a+b}`;
+// }
+
+var pi = 3.14;
+
+//抛出 
+// module.exports.counter = counter;
+// module.exports.adder = adder;
+// module.exports.pi = pi;
+
+//简写
 module.exports = {
-    counter: function (arr) {
-        return 'three are' + arr.length + 'elements in  the array';
+    counter: function(arr) {
+        return "There are " + arr.length + " element in the array"
     },
-    adder: adder,
-    pi: pi
+    adder: adder = function(a) {
+        return `the sum of 2 number is ${a+pi}`;
+    },
+    // pi:pi
 }
