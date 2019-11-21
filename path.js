e62650a3233789c40ef123032b7451e8b571fc17
unknown <taoxf@ipharmacare.net>
2019-11-22 00:32:04 +0800

    node 基本API

---
+++ path.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+// console.log(path)
+
+let str = 'F:/LDSGameMaster/LDSGamePlayerPK/a.jpg';
+
+
+//dirname
+//basename
+//extname
+console.log(path.dirname(str));
+console.log(path.basename(str));
+console.log(path.extname(str));
