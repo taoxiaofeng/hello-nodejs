3d6ad41ddca3df37ffc9599e436ce9abface2abc
taoxf <taoxf@ipharmacare.net>
2019-11-24 15:31:33 +0800

    nodeJs 实现注册登录

--- server/server6.js
+++ server/server6.js
@@ -21,18 +21,51 @@ let server = http.createServer((req, res) => {
 
     req.on('end', () => {
         let post = querystring.parse(str);
+        let { user, pass } = query;
 
         // console.log(pathname, query, post)
 
         //写东西
         switch (pathname) {
             case '/reg':    //  注册
+                if (!user) {
+                    res.write('{"err":1, "msg": "user必须有"}');
+                } else if (!pass) {
+                    res.write('{"err": 1, "msg": "pass必须有"}');
+                } else if (!/^\w{8,32}$/.test(user)) {
+                    res.write('{"err": 1, "msg": "user非法"}');
+                } else if (/^['|"]$/.test(pass)) {
+                    res.write('{"err": 1, "msg": "pass非法"}');
+                } else if (users[user]) {
+                    res.write('{"err": 1, "msg": "此用户名已经被占用"}');
+                } else {
+                    users[user] = pass;
+                    res.write('{"err": 0, "msg": "注册成功"}');
+                }
+                res.end();
                 break;
             case '/login':  // 登录
+               
+                if (!user) {
+                    res.write('{"err":1, "msg": "user is required"}');
+                } else if (!pass) {
+                    res.write('{"err": 1, "msg": "pass is required"}');
+                } else if (!/^\w{8,32}$/.test(user)) {
+                    res.write('{"err": 1, "msg": "invaild user"}');
+                } else if (/^['|"]$/.test(pass)) {
+                    res.write('{"err": 1, "msg": "invaild pass"}');
+                } else if (!users[user]) {
+                    res.write('{"err": 1, "msg": "no this user"}');
+                } else if (users[user] != pass) {
+                    res.write('{"err":1, "msg": "username or password is incorrect"}');
+                } else {
+                    res.write('{"err": 0, "msg": "login success"}');
+                }
+                res.end();
                 break;
             default:        // 其他；文件
                 fs.readFile(`../www${pathname}`, (err, data) => {
-                    if(err) {
+                    if (err) {
                         res.writeHeader(404);
                         res.write('Not Found');
                     } else {
